1e80dc0c769d6918eae0189536409ff218731572
Burn-the-asses <shahnawazwarriach2323@gmail.com>
2022-03-18 12:41:38 +0500

    Action Parameters testing

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -13,11 +13,20 @@ namespace VidlyV3.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
-            //return View(movie);
-            //return Content("Hello World!");
-            //return HttpNotFound();
-            //return new EmptyResult();
-            return RedirectToAction("Index", "Home",new {page = 1 , sortBy = "name"});
+            return View(movie);
+        }
+        public ActionResult Edit(int movieid)
+        {
+            return Content("id=" + movieid);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content(string.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
     }
 }
